ee7d46cb1c2263e414c23f195bc392307412b05f
Akumachan <akumachanit@gmail.com>
2021-09-28 19:25:38 +0900

    Update dist directory

--- dist/api-client.d.ts
+++ dist/api-client.d.ts
@@ -19,7 +19,7 @@ declare type Method<A extends ApiScheme, S extends string | symbol | number> = {
     [I in keyof A]: S extends I ? keyof A[I] extends MethodList ? keyof A[I] : never : never;
 }[keyof A];
 declare type GetType<A extends ApiScheme, K> = K extends keyof A ? A[K] : undefined;
-export declare const ApiClient: (url: `http${string}/`) => {
+export declare const ApiClient: (url: `http${string}`) => {
     service: <T extends ApiScheme>() => {
         call: <P extends keyof T, M extends Method<T, P>, Parameters_1 extends GetType<T[P][M], "parameters">, Response_1 extends GetType<T[P][M], "response">>(path: P, method: M, parameters: Parameters_1) => Promise<Response_1>;
     };
